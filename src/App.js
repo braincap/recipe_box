6f7c92683923d84087bf0d6c4564fd736072789f
Jai Tiwari <contactjaitiwari@gmail.com>
2017-03-13 02:24:58 -0400

    Slider and panel added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+var $ = require('jquery');
 
 var RecipeList = React.createClass({
   getInitialState: function () {
@@ -9,19 +10,24 @@ var RecipeList = React.createClass({
   },
   render: function () {
     return (
-      <div>
-        <ul className="list-unstyled">
-          {this.state.recipes.map(item => <li key={item}><Recipe item={item} /></li>)}
-        </ul>
+      <div className="recipe-list">
+        {this.state.recipes.map(item => <Recipe key={item} item={item} />)}
       </div>
     )
   }
 });
 
 var Recipe = React.createClass({
+  handleClick: function (e) {
+    $('.panel-body').not($(e.currentTarget).next()).slideUp(200);
+    $(e.currentTarget).next().slideToggle(200);
+  },
   render: function () {
     return (
-      <div className="recipe well">{this.props.item}</div>
+      <div className="recipe panel panel-success">
+        <div className="panel-heading" onClick={this.handleClick}>{this.props.item}</div>
+        <div className="panel-body">This is the recipe of {this.props.item}</div>
+      </div>
     )
   }
 })
